acc778f678f907d3e1d838263730ff627dfe9284
Chris Palmer <snackypants@gmail.com>
2022-11-06 11:42:31 -0800

    Tidy up follow-everybody.bash (#9)
    Co-authored-by: Chris Palmer <chris@noncombatant.org>

--- follow-everybody.bash
+++ follow-everybody.bash
@@ -1,9 +1,11 @@
+#!/usr/bin/env bash
+
 # Copyright @datn 2022. Heavens forgive me for this
 # be in your repo dir for this
-for I in `curl -H "Accept: application/vnd.github+json" https://api.github.com/repos/diracdeltas/tweets/forks | jq -r '.[].full_name'`
+for I in $(curl -H "Accept: application/vnd.github+json" https://api.github.com/repos/diracdeltas/tweets/forks | jq -r '.[].full_name')
 do  
-	USEW=`echo $I | cut -d\/ -f1`
-	WEPO=`echo $I | cut -d\/ -f2`
-	git remote add $USEW https://github.com/$USEW/$WEPO
-	git fetch $USEW
+	user=$(echo "$I" | cut -d\/ -f1)
+	repo=$(echo "$I" | cut -d\/ -f2)
+	git remote add "$user" "https://github.com/$user/$repo"
+	git fetch "$user"
 done
